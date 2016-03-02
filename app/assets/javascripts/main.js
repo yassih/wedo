9fff9e05ccfb71737a82dd39ceb85ca2b42822df
Yassi <yassih84@gmail.com>
2016-03-01 19:14:12 -0500

    changed navbar just a bit, the toggle now works on list items

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,17 +1,30 @@
 var items = [];
+
 $(document).ready(function(){
 	$('.form-inputs > .items').keypress(function(event){
 		if(event.which == 13){
-			console.log('hey');
-			items.push( $('#item_name').val());
-			
+			// regular expression replacing all single spaces with ""
+			if($('#item_name').val().replace(/\s/g, "") === ''){
+				// alert('you need to add an item');
+				return;
+			} 
+			var input = $('#item_name').val();
+			items.push(input);
+
 			var myList = $('.list-group');
 			var newItem = $('<li class="list-group-item"></li>');
-			newItem.html($('#item_name').val());
+			newItem.html("<i class='glyphicon glyphicon-unchecked'></i> " + input);
 			myList.append(newItem);
-			$('#item_name').val(' ');
-		}
 
+			$('#item_name').val(' ');
+
+			newItem.on('click', function(){	
+				$(this).find('i')
+					.toggleClass('glyphicon-unchecked')
+					.toggleClass('glyphicon-check');
+			 });
+
+		}
 
 	});
 });
--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -72,6 +72,6 @@ class ListsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def list_params
-      params.require(:list).permit(:name, :date_completed, :due_date, :category_id)
+      params.require(:list).permit(:name, :due_date, :category_id)
     end
 end

fd56a5b1b835817ce4b549e2e5783638328c7f1c
Yassi <yassih84@gmail.com>
2016-03-08 09:44:26 -0500

    added seed for category

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+c = Category.create([
+	{   name: "Grocery"},
+	{   name: "Bills"},
+	{   name: "Chores"}
+	])

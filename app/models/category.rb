ed9977a561c669c7fb725445b4f3e3477d55d58f
Yassi <yassih84@gmail.com>
2016-03-28 12:58:15 -0400

    validaatons for categories

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,5 @@
 class Category < ActiveRecord::Base
 	has_many :lists
-	validates_uniqueness_of :name
+	validates_uniqueness_of :name , :case_sensitive => false
 	validates :name, presence: true
 end
